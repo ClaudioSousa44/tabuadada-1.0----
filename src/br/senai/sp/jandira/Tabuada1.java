3180eacf3d404436a61e60341b9a4d254a5aa330
Claudio Sousa <cauhsousa@gmail.com>
2022-09-01 16:54:21 -0300

    primeiro teste

--- src/br/senai/sp/jandira/Tabuada1.java
+++ src/br/senai/sp/jandira/Tabuada1.java
@@ -3,7 +3,7 @@ package br.senai.sp.jandira;
 public class Tabuada1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/br/senai/sp/jandira/model/Multiplicacao.java
+++ src/br/senai/sp/jandira/model/Multiplicacao.java
@@ -1,5 +1,29 @@
 package br.senai.sp.jandira.model;
 
 public class Multiplicacao {
+	
+	public double multiplicando;
+	public double minMult;
+	public double maxMult;
+	public double res;
+	
+	public double getTabuada() {
+		while(minMult <= maxMult) {
+			res = multiplicando * minMult;
+			minMult++;
+			
+		}
+		return res;
+		
+	}
+	
+	public String getResultado() {
+		
+		while(minMult <= maxMult) {
+			String exibir =  multiplicando + "X" + minMult + "=" + res;
+			minMult++;
+		}
+		return exibir;
+	}
 
 }
